40d24cc07f5387eceabcb942a3af89ba92cc8ea2
Felisdoro <125565243+Felisdoro@users.noreply.github.com>
2023-04-17 22:30:49 -0300

    Add files via upload

---
+++ Clinica_Psicologia_TCC-main/pedro/README.md
@@ -0,0 +1,2 @@
+# Clinica_Empathia_TCC
+Repositório criado para armazenar e controlar o desenvolvimento do Projeto Clinica de Psicologia/Psicanálise/Psiquiatria.
---
+++ Clinica_Psicologia_TCC-main/pedro/clinica_psicologia_tcc-main/login.js
@@ -0,0 +1,11 @@
+function logar(){
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+
+    if(login == "admin" && senha == "admin"){
+        alert('Sucesso');
+        location.href = "home.html";
+    }else{
+        alert('Usuario ou senha incorreto');
+    }
+}
