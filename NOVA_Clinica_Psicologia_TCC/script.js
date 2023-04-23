30733c35c64095783690cb3828f000d83207345f
Felisdoro <125565243+Felisdoro@users.noreply.github.com>
2023-04-23 12:05:29 -0300

    Add files via upload

---
+++ NOVA_Clinica_Psicologia_TCC/script.js
@@ -0,0 +1,10 @@
+/* javaScript para acessar o menu responsivo da navbar*/
+var navLins = document.getElementById("navLinks");
+
+function showMenu(){
+    navLinks.style.right = "0";
+}
+function hideMenu(){
+    navLinks.style.right = "-200px";
+}
+/* javaScript para acessar o menu responsivo da navbar*/
