340a000ead49e8cc6585b67c969832400de659e1
Pedro Almeida <112005029+pedroalmeida777@users.noreply.github.com>
2023-06-02 09:12:04 -0300

    Add Banco de Dados Rev.1

--- bdEmpathia_Clinica.sql
+++ bdEmpathia_Clinica.sql
@@ -8,6 +8,43 @@ GO
 USE bdEmpathiaClinica
 GO
 
+
+CREATE TABLE Login
+(
+	email	VARCHAR(100) NOT NULL,
+	senha	VARCHAR(50)	 NOT NULL
+)
+GO
+
+
+
+
+CREATE TABLE Cadastro
+(
+	id		INT IDENTITY PRIMARY KEY,
+    usuario_id			 INT, 
+    nome					 VARCHAR(100) NOT NULL,
+    rg					 VARCHAR(11)  NOT NULL, 
+    cpf					 VARCHAR(11)  NOT NULL,
+    sexo					 VARCHAR(10)	  NULL,
+    data_nasc			 VARCHAR(11)  NOT NULL,
+    telefone				 VARCHAR(20)  NOT NULL,
+    email				 VARCHAR(150) NOT NULL,
+    logradouro			 VARCHAR(100) NOT NULL,
+    numero				 VARCHAR(5) NOT NULL,
+    complemento			 VARCHAR(100) NOT NULL,
+    bairro				 VARCHAR(100) NOT NULL,
+    cidade				 VARCHAR(100) NOT NULL,
+    estado				 VARCHAR(100) NOT NULL, 
+    cep					 VARCHAR(10) NOT NULL,
+    servico_modalidade	 VARCHAR(100) NOT NULL,
+    servico_seguimento	 VARCHAR(100) NOT NULL,	
+
+)
+GO
+
+
+
 -- TABELA USUÁRIO
 CREATE TABLE Usuario
 ( 
@@ -48,45 +85,41 @@ GO
 
 CREATE TABLE Funcionario
 (
-   id			 INT IDENTITY PRIMARY KEY,
-   usuario_id	 INT,
-   nome          VARCHAR(100) NOT NULL,
-   rg			 VARCHAR(11)  NOT NULL, 
-   cpf			 VARCHAR(11)  NOT NULL,
-   sexo			 VARCHAR(10)	  NULL,
-   data_nasc	 VARCHAR(11)  NOT NULL,
-   telefone		 VARCHAR(20)  NOT NULL,
-   email		 VARCHAR(150) NOT NULL,
-   logradouro    VARCHAR(100) NOT NULL,
-   numero        VARCHAR(5)   NOT NULL,
-   complemento   VARCHAR(100) NOT NULL,
-   bairro        VARCHAR(100) NOT NULL,
-   cidade        VARCHAR(100) NOT NULL,
-   estado        VARCHAR(100) NOT NULL, 
-   cep           VARCHAR(10)  NOT NULL,
-   cargo_id		 VARCHAR(100) NOT NULL,
-   cargo_func    VARCHAR(100) NOT NULL, -- Atendente, Psicologo, Psiquiatra, Psicanalista
-   modalidade	 VARCHAR(100) NOT NULL,
-   seguimento	 VARCHAR(100) NOT NULL,
-   salario		 VARCHAR(50)  NOT NULL
+   id			    INT IDENTITY PRIMARY KEY,
+   usuario_id	    INT,
+   nome             VARCHAR(100) NOT NULL,
+   rg			    VARCHAR(11)  NOT NULL, 
+   cpf			    VARCHAR(11)  NOT NULL,
+   sexo			    VARCHAR(10)	  NULL,
+   data_nasc		VARCHAR(11)  NOT NULL,
+   telefone			VARCHAR(20)  NOT NULL,
+   email			VARCHAR(150) NOT NULL,
+   logradouro		VARCHAR(100) NOT NULL,
+   numero			VARCHAR(5)   NOT NULL,
+   complemento		VARCHAR(100) NOT NULL,
+   bairro			VARCHAR(100) NOT NULL,
+   cidade			VARCHAR(100) NOT NULL,
+   estado			VARCHAR(100) NOT NULL, 
+   cep				VARCHAR(10)  NOT NULL,
+   cargo_id			VARCHAR(100) NOT NULL,
+   cargo_func		VARCHAR(100) NOT NULL, -- Atendente, Psicologo, Psiquiatra, Psicanalista
+   status_func		VARCHAR(50) NOT NULL, -- ATIVO/CONTRATADO/ATUANDO E INATVIO/DEMITIDO/DESCONTRATADO
+   cargo_modalidade	VARCHAR(100) NOT NULL,
+   seguimento		VARCHAR(100) NOT NULL,
+   salario			VARCHAR(50)  NOT NULL
 
    FOREIGN KEY (usuario_id) REFERENCES Usuario (id),
    FOREIGN KEY (cargo_id) REFERENCES Cargo (id),
-   FOREIGN KEY (cargo_func) REFERENCES Cargo (func)
-
+   FOREIGN KEY (cargo_func) REFERENCES Cargo (cargo)
 )
 GO
 
 CREATE TABLE Cargo 
 (
 	id					INT IDENTITY PRIMARY KEY,
-	func				VARCHAR(100) NOT NULL,
-	status_func			VARCHAR(50) NOT NULL, -- ATIVO/CONTRATADO/ATUANDO E INATVIO/DEMITIDO/DESCONTRATADO
-	crm					VARCHAR(50) NOT NULL, --APENAS PSIQUIATRA
-	crp					VARCHAR(50) NOT NULL, --APENAS PSICOLOGO
-	servico_modalidade	VARCHAR(100) NOT NULL,
-	servico_seguimento	VARCHAR(100) NOT NULL
-
+	cargo				VARCHAR(100) NOT NULL,
+	modalidade			VARCHAR(100) NOT NULL,
+	seguimento			VARCHAR(100) NOT NULL,
 
 )
 GO
@@ -94,14 +127,20 @@ GO
 CREATE TABLE Mensalidade 
 (
 	id						INT IDENTITY PRIMARY KEY,
+	valor					VARCHAR(50)	 NOT NULL,
+	dia_pagamento			VARCHAR(10)	 NOT NULL,
 	servico_modalidade		VARCHAR(100) NOT NULL,
-	servico_tipo			VARCHAR(150) NOT NULL,
+	servico_tipo			VARCHAR(150) NOT NULL
 )
 
 CREATE TABLE Servico 
 (
 	id				INT IDENTITY PRIMARY KEY,
+	usuario_id		INT NOT NULL,
 	tipo			VARCHAR(150) NOT NULL,
 	profissional	VARCHAR(100) NOT NULL,
 
+
+	FOREIGN KEY (usuario_id) REFERENCES Usuario (id),
+    FOREIGN KEY (funcionario_id) REFERENCES Funcionario (id),
 )
